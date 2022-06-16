6191a63b9f110939049d8b083744a20191fb4202
Aniebiet Francis <francisaniebiet678@gmail.com>
2022-06-16 15:28:04 +0100

    changes

--- home/models.py
+++ home/models.py
@@ -46,8 +46,6 @@ class Product(models.Model):
   product_name = models.CharField(max_length=220)  
   product_price = models.CharField(max_length=220)  
   product_category = models.CharField(max_length=220)  
-  
-  def __str__(self):
-        return self.product_name
+
       
 
