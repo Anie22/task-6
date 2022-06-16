58e38360dce3cd84b7257406cb3a726e15312ace
Aniebiet Francis <francisaniebiet678@gmail.com>
2022-06-16 15:29:35 +0100

    changes

--- task/task7.py
+++ task/task7.py
@@ -1,11 +1,23 @@
-from itertools import Product
+from email.policy import default
+from itertools import Product, product
 from home.models import Product
 
-class Product(models.Model):
-    product_name = models.CharField(max_length=220)  
-    product_price = models.CharField(max_length=220)  
-    product_category = models.CharField(max_length=220)  
-  
-    def __str__(self):
-        return self.product_name
-      
+# Inserting a new record into the Product Model
+
+product2 = Product(product_name="Rice", product_type="Cereals", product_category="Grains", product_price="50,000")
+product2.save()
+
+# Get all the records in the Product table
+all_entries = Product.objects.all()
+all_entries
+
+# Filtering products by their name     
+x = product.objects.filter(product_name="Rice")
+
+# Getting a single record from the product table
+one_entry = product.objects.get(pk=1)
+
+
+# Changing a product price
+x = product_price = "100,000"
+x.save()
